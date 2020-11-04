dbf0d02a6331cd48b136aa694672263cbba8fca6
Sven <svenpubmail@gmail.com>
2020-11-04 11:57:13 -0500

    started ch4

--- other/refresh_chapters.R
+++ other/refresh_chapters.R
@@ -13,3 +13,11 @@ lapply(
   render,
   output_dir = here::here('ISL')
 )
+
+f = function(x){exp(x)/(1+exp(x))}
+g = function(x){x/(1-x)}
+x = seq(from = 0, to = 1, by = 0.01)
+y = f(x)
+y2 = g(x)
+par(mfrow = c(1,1))
+plot(x,y2)

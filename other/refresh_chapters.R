b3eab09bb160454d27da63f2d583fc7c0ff7765b
Sven <svenpubmail@gmail.com>
2020-11-12 09:45:39 -0500

    starting ch4 exercises

--- other/refresh_chapters.R
+++ other/refresh_chapters.R
@@ -14,10 +14,3 @@ lapply(
   output_dir = here::here('ISL')
 )
 
-f = function(x){exp(x)/(1+exp(x))}
-g = function(x){x/(1-x)}
-x = seq(from = 0, to = 1, by = 0.01)
-y = f(x)
-y2 = g(x)
-par(mfrow = c(1,1))
-plot(x,y2)
